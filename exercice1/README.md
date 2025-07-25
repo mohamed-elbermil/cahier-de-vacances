60cef14fbe8fc42f50ab6c42a4ccd44184987ae5
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2025-07-25 13:15:38 +0200

    remise de l'exo

---
+++ exercice1/README.md
@@ -0,0 +1,21 @@
+ <!-- Explication de vos choix, de vos éventuelles difficultés ou de ce que vous avez appris. -->
+
+Explication de mes choix :
+
+- Pour la soustraction, j'ai ajouté une condition qui affiche un message d'erreur lorsque le premier nombre est plus petit que le second. Dans le but qu'il ne retourne pas de valeur négatif
+
+- Pour la division, une condition pour ne pas diviser par zéro car il n'y a pas vraiment d'intéret.
+
+---
+
+Difficulté :
+
+- J'ai eu un peu de mal avec les fermetures "rl.close()" qui n'était pas bien placé
+
+---
+
+Ce que j'ai appris :
+
+- Avoir une interraction avec le terminal de cette manière, car d'habitude je teste dans le navigateur avec prompt. Donc là, c'est nouveau pour moi.
+
+- Je n'ai pas eu le reflex de convertir la chaîne de caractère en décimal pour les 2 nombres saisi en console. J'ai appris que ce que l'utilisateur saisi dans le terminal, est du string même s'il tape des chiffres.cd
--- exercice1/script.js
+++ exercice1/script.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const readline = require("readline");
 
 // Création de l'interface pour la console
@@ -12,22 +13,30 @@ console.log("2/ Soustraction");
 console.log("3/ Multiplication");
 console.log("4/ Division");
 
-// Demande de calcul souhaité
+// Demande de opération souhaité et 2 nombres à calculer
 rl.question("Choisissez une option :", (option) => {
   rl.question("Entrer le premier nombre : ", (num1) => {
     rl.question("Entrer le deuxième nombre : ", (num2) => {
-      // Convertie la chaîne de caractère en décimal pour les 2 nombres saisi
+      // Convertir la chaîne de caractère en décimal pour les 2 nombres saisi
       const a = parseFloat(num1);
       const b = parseFloat(num2);
       let result;
 
-      // Condition pour gérer quelle type de calcul retourné en fonction de l'option choisis (via le numéro)
+      // Condition pour gérer quelle type de calcul retourner en fonction de l'option choisi (via le numéro)
       switch (option) {
         case "1":
           result = a + b;
           break;
         case "2":
-          result = a - b;
+          if (a > b) {
+            result = a - b;
+          } else {
+            console.log(
+              "Erreur : Le deuxième nombre est plus grand que le premier"
+            );
+            rl.close(); // Fermeture de l'interface
+            return;
+          }
           break;
         case "3":
           result = a * b;
@@ -36,8 +45,7 @@ rl.question("Choisissez une option :", (option) => {
           if (b !== 0) {
             result = a / b;
           } else {
-            console.log("Erreur : Ne pas diviser par séro");
-            // Fermeture de l'interface
+            console.log("Erreur : Ne pas diviser par zéro");
             rl.close();
             return;
           }
@@ -47,6 +55,8 @@ rl.question("Choisissez une option :", (option) => {
           rl.close();
           return;
       }
+      console.log(`Résultat : ${result}`);
+      rl.close();
     });
   });
 });
